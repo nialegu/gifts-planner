6cb6daf7de21a68fef45ddae1441d10faea8498a
Leonid Gusev <rastrast207@gmail.com>
2024-03-29 16:35:56 +0300

    work with radio buttons and insert

--- app/src/main/java/com/example/android/trackmysleepquality/clothesform/ClothesFormFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/clothesform/ClothesFormFragment.kt
@@ -5,14 +5,20 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import androidx.core.view.get
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.database.Clothes
 import com.example.android.trackmysleepquality.database.SleepDatabase
 import com.example.android.trackmysleepquality.databinding.FragmentClothesFormBinding
 import com.example.android.trackmysleepquality.databinding.FragmentSleepTrackerBinding
+import com.example.android.trackmysleepquality.enums.ClothesSize
+import com.example.android.trackmysleepquality.enums.Season
+import com.example.android.trackmysleepquality.enums.Type
 import com.example.android.trackmysleepquality.sleeptracker.SleepTrackerViewModel
 import com.example.android.trackmysleepquality.sleeptracker.SleepTrackerViewModelFactory
 
@@ -35,10 +41,46 @@ class ClothesFormFragment : Fragment() {
         viewModel = ViewModelProvider(this, viewModelFactory)
             .get(ClothesFormViewModel::class.java)
 
-        binding.addButton.setOnClickListener {
-            val name = binding.nameField.editText?.text.toString()
-            val description = binding.descriptionField.editText?.text.toString()
+        var name: String
+        var season: Season = Season.summer
+        var type: Type = Type.top
+        var description: String
+        var clothesSize: ClothesSize? = null
+        var shoesSize: Int? = null
 
+        binding.addButton.setOnClickListener {
+            name = binding.nameField.editText?.text.toString()
+            description = binding.descriptionField.editText?.text.toString()
+            val newClothes: Clothes = Clothes(
+                name = name,
+                season = season,
+                type = type,
+                description = description,
+                clothesSize = clothesSize,
+                shoesSize = shoesSize
+            )
+            viewModel.insertNewClothes(newClothes)
+            this.findNavController().navigate(R.id.action_clothesFormFragment_to_sleepTrackerFragment)
+        }
+
+        binding.season.setOnCheckedChangeListener { group, checkedId ->
+            val radio: RadioButton = group.findViewById(checkedId)
+            when (radio.text){
+                "summer" -> season = Season.summer
+                "winter" -> season = Season.winter
+                "demiseason" -> season = Season.demiseason
+            }
+        }
+
+        binding.type.setOnCheckedChangeListener { group, checkedId ->
+            val radio: RadioButton = group.findViewById(checkedId)
+            when (radio.text){
+                "Top" -> type = Type.top
+                "Bottom" -> type = Type.bottom
+                "Shoes" -> type = Type.shoes
+                "Headdress" -> type = Type.headdress
+                "Accessories" -> type = Type.accessories
+            }
         }
 
         return binding.root
--- app/src/main/java/com/example/android/trackmysleepquality/clothesform/ClothesFormViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/clothesform/ClothesFormViewModel.kt
@@ -7,6 +7,7 @@ import com.example.android.trackmysleepquality.database.SleepDatabaseDao
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class ClothesFormViewModel(
@@ -16,6 +17,12 @@ class ClothesFormViewModel(
     private var viewModelJob = Job()
     private val uiScope = CoroutineScope(Dispatchers.Main +  viewModelJob)
 
+
+    fun insertNewClothes(clothes: Clothes){
+        uiScope.launch {
+            insert(clothes)
+        }
+    }
     private suspend fun insert(clothes: Clothes) {
         withContext(Dispatchers.IO) {
             dao.insertClothes(clothes)
